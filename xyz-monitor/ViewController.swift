a380f9b14f42719dbc49cf4e7f79cbf50c3eee65
Aarti Chella <aartichella@gmail.com>
2019-03-11 15:28:12 -0400

    2nd commit

---
+++ xyz-monitor/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  xyz-monitor
+//
+//  Created by Aarti Chella on 3/11/19.
+//  Copyright © 2019 aarti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
